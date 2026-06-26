6d46fe423d5137c718b11a06bbe052b693b4d03f
dharma0907 <dharma.akdm@gmail.com>
2026-06-26 22:12:30 +0530

    added redis and rabbitmq route53

--- 40-database/route53.tf
+++ 40-database/route53.tf
@@ -7,3 +7,21 @@ resource "aws_route53_record" "mongodb" {
   ttl     = 1
   records = [aws_instance.mongodb.private_ip]
 }
+
+#redis-dev-dcinema.online
+resource "aws_route53_record" "redis" {
+  zone_id = var.zoneid
+  name    = "redis-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.redis.private_ip]
+}
+
+#rabbitmq-dev-dcinema.online
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zoneid
+  name    = "rabbitmq-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.rabbitmq.private_ip]
+}
