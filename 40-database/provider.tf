028b78f5e58e7a66b8aa0f1bd41568ad6762b46f
dharma0907 <dharma.akdm@gmail.com>
2026-06-29 18:53:52 +0530

    modified files

--- 40-database/provider.tf
+++ 40-database/provider.tf
@@ -12,7 +12,7 @@ terraform {
 terraform {
   backend "s3" {
     bucket = "dharma-90"
-    key    = "roboshop-dabase-instances.tfstate"
+    key    = "roboshop-database-instances.tfstate"
     region = "us-east-1"
     encrypt = true
     use_lockfile = true # will enable locking
