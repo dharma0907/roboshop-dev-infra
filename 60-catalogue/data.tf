06039a4c309fc807c975db25e799690bf9a413c4
dharma0907 <dharma.akdm@gmail.com>
2026-07-01 18:15:22 +0530

    adding files

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -2,9 +2,9 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
     name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
 # we are creating catalogue instance in private subnet
-data "aws_ssm_parameter" "private_subnet_id" {
+data "aws_ssm_parameter" "private_subnet_ids" {
 
-  name = "/${var.project}/${var.environment}/private_subnet_id"
+  name = "/${var.project}/${var.environment}/private_subnet_ids"
 }
 
 data "aws_ssm_parameter" "vpc_id" {
