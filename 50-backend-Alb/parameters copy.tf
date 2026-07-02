5d1e6e92cdf6979d87d7507695553452fd4366e6
dharma0907 <dharma.akdm@gmail.com>
2026-07-02 12:42:16 +0530

    adding files

---
+++ 50-backend-Alb/parameters copy.tf
@@ -0,0 +1,6 @@
+# storing backend alb arn in parameter store
+resource "aws_ssm_parameter" "backend_alb_listener" {
+  name = "/${var.project}/${var.environment}/backend_alb_listener"
+  type = "String"
+  value = aws_lb_listener.http.arn
+  overwrite = true
