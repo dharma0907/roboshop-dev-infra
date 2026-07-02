b82b7444604d47b4dd316172e3aef088ce45cda8
dharma0907 <dharma.akdm@gmail.com>
2026-07-02 17:01:19 +0530

    adding files

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -5,7 +5,7 @@ resource "aws_acm_certificate" "roboshop" {
   tags = merge(
     local.common_tags,
     {
-        Name = "${var.project}-${var.environment}-certificate}"
+        Name = "${var.project}-${var.environment}-certificate"
     }
   )
   lifecycle {
