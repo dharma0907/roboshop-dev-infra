7ba89bf68dda1e2d7cf2e04db30074bf60e92eb0
dharma0907 <dharma.akdm@gmail.com>
2026-07-02 10:59:53 +0530

    adding files

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "catalogue" {
   
   tags = merge(
     {
-        Name = "${local.common_name}-catalogue"
+        Name = "${local.common_name}-catalogue-${var.app_version}-${var.environment}"
     },
     local.common_tags
   )
@@ -45,7 +45,7 @@ resource "aws_ec2_instance_state" "catalogue" {
   depends_on = [ terraform_data.catalogue ] # we are saying stop only after creating and configuring instance, so 1 and 2 should manadetory to run 3
 }
 
-# # 4. Take or create ami template
+# # 4. Take or create ami template, this ami temolate we are taking form existing instance
 resource "aws_ami_from_instance" "catalogue" {
   name               = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
   source_instance_id = aws_instance.catalogue.id
@@ -57,4 +57,47 @@ resource "aws_ami_from_instance" "catalogue" {
     local.common_tags
   )
 
+}
+
+
+#launch template
+resource "aws_launch_template" "catalogue" {
+  name = "${local.common_name}-catalogue-launch-template"
+  image_id = aws_ami_from_instance.catalogue.id # we are taking from current instance
+  instance_initiated_shutdown_behavior = "terminate"
+  update_default_version = true
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.catalogue_sg_id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+     tags = merge(
+         {
+           Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+       },
+    local.common_tags
+   )
+
+  }
+
+  # with onstance it will create a EBS volume, we can add tags for volume as well
+   tag_specifications {
+    resource_type = "volume"
+
+     tags = merge(
+         {
+           Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+       },
+    local.common_tags
+   )
+  }
+#launch template tags
+     tags = merge(
+         {
+           Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+       },
+    local.common_tags
+   )
+ 
 }
